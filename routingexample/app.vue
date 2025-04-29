e0d198ba0e7062d9739ed9b2d6654e95fa95314a
Dori Fdez <md.fernandez.sanchez@gmail.com>
2025-04-29 14:39:17 +0200

    Subiendo proyecto ejemplo router

---
+++ routingexample/app.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <NuxtLayout>
+      <NuxtPage />
+      <nav>
+        <ul>
+          <li><NuxtLink to="/">Home</NuxtLink></li>
+          <li><NuxtLink to="/about">About</NuxtLink></li>
+          <li><NuxtLink to="/services">Services</NuxtLink></li>
+          <li><NuxtLink to="/contact">Contact</NuxtLink></li>
+          <li><NuxtLink to="/projects">Proyectos</NuxtLink></li>
+        </ul>
+      </nav>
+    </NuxtLayout>
+
+  </div>
+</template>
+
+<script setup>
+</script>
+
---
+++ routingexample/nuxt.config.ts
@@ -0,0 +1,32 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true },
+  ssr:true,
+
+  /*patron JAMstack
+  nitro:{
+    prerender: {
+      routes:['/services', '/contact', '/about', '/']
+    }
+  }
+
+  Hybrido
+  routeRules:{
+  '/':{ prepender: true},
+  '/about': { ssr:true},
+  '/services' :{ ssr:false}
+  }
+
+*/
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/content',
+    '@nuxt/fonts',
+    '@nuxt/icon',
+    '@nuxt/image',
+    '@nuxt/scripts',
+    '@nuxt/test-utils',
+    '@nuxt/ui'
+  ]
+})
